122bdd10233e84a59b0f8a6e4130f37ee99c96c4
shivamsharma006 <147228448+shivamsharma006@users.noreply.github.com>
2023-10-15 22:07:14 +0530

    Add files via upload

---
+++ sumOfDigits.java
@@ -0,0 +1,15 @@
+package javacoding;
+public class sumOfDigits{
+    public static int sumOfDigits(int num){
+        if(num==0){
+            return 0;
+        }
+        return (num%10)+sumOfDigits(num/10);
+    }
+    public static void main(String[] args) {
+        int num = 12345;
+        int result = sumOfDigits(num);
+        System.out.println(result);
+
+    }
+}
