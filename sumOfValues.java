23ebc923fffa08931ad378237e7caeb0fc6288c2
shivamsharma006 <147228448+shivamsharma006@users.noreply.github.com>
2023-10-18 19:18:26 +0530

    Add files via upload

---
+++ sumOfValues.java
@@ -0,0 +1,15 @@
+//PW CLASSES
+package javacoding;
+public class sumOfValues{
+    public static int totalSum(int []arr , int idx){
+        if(idx == 0)return arr[idx];
+        
+        return arr[idx] + totalSum(arr , idx - 1);
+    }
+    public static void main(String[] args){
+        int arr[] = {92,23, 15, -20, 10};
+        int n = arr.length;
+        
+        System.out.println("The sum of all elements of the array is : " + totalSum(arr , n - 1));
+    }
+}
