258490fbf426dd905cfdd405cb2844a9d9972d77
shivamsharma006 <147228448+shivamsharma006@users.noreply.github.com>
2024-06-04 22:33:14 +0530

    binaryRecursion.java

--- binaryRecursion.java
+++ binaryRecursion.java
@@ -1,32 +1,35 @@
-//BINARY SEARCH USING RECURSION
-//PW JAVA(RECURSION PLAYLIST)
-package javacoding;
+import java.util.Scanner;
 
-public class binaryRecursion {
-    //function defination
-    public static int binarySearch(int arr[], int low, int high, int x) {
-        int result = -1;
-        while (low <= high) {
+public class binarySearchRecursion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER THE SIZE OF ARRAY: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the array elements:");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int low = 0;
+        int high = n - 1;
+        System.out.println("ENTER THE NO WHICH U WANT TO FIND:");
+        int x = sc.nextInt();
+        int result = binarySearch(arr, low, high, x);
+        System.out.println("At index number:" + result);
+    }
+
+    public static int binarySearch(int[] arr, int low, int high, int x) {
+        while(low <= high) {
             int mid = low + (high - low) / 2;
             if (arr[mid] == x) {
                 return mid;
-            } else if (arr[mid] < x) {
-                //traverse towards right side of an array
-                return binarySearch(arr, mid + 1, high, x);
-            } else {
-                //traverse towards left side of an array
+            } else if (arr[mid] > x) {
                 return binarySearch(arr, low, mid - 1, x);
+            } else {
+                return binarySearch(arr, mid + 1, high, x);
             }
         }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        int arr[] = { 2, 4, 8, 10, 12, 22, 24 };
-        int low = 0;
-        int high = arr.length - 1;
-        int x = 22;
-        int result = binarySearch(arr, low, high, x);
-        System.out.println(result);
+        return -1; // Element not found
     }
 }
+
